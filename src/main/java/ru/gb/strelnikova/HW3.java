22faa129a2bcce214f1831ad3ce3ccb3f68ac157
Maria <369aksi@gmail.com>
2021-10-22 14:40:25 +0300

    First task done

--- src/main/java/ru/gb/strelnikova/HW3.java
+++ src/main/java/ru/gb/strelnikova/HW3.java
@@ -1,7 +1,17 @@
 package ru.gb.strelnikova;
 
+import java.util.Arrays;
+
 public class HW3 {
     public static void main(String[] args) {
-
+        int[] nums = {  1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+        for (int i = 0; i < 10; i++) {
+            if (nums[i] == 1) {
+                nums [i] = 0;
+        } else {
+                nums [i] = 1;
+            }
+    }
+    System.out.println(Arrays.toString(nums));
     }
 }
